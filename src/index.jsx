1e38f43cb7222eabdfb0fcf0c451a69af5eb6cca
Frityet <ambhogal01@gmail.com>
2024-04-06 19:47:40 -0700

    add analytics

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Analytics } from "@vercel/analytics/react"
 
 const darkTheme = createTheme({
   palette: {
@@ -17,6 +18,7 @@ root.render(
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
